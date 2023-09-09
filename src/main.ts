1fd9f11d70f5da5e9047e4420cf8a5ccbccd6a25
ykob <info@tplh.net>
2023-09-10 01:10:39 +0900

    removed the default files,

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import EditorJS from "@editorjs/editorjs";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const editor = new EditorJS({
+  holder: "app-editor",
+  defaultBlock: "paragraph",
+  i18n: {
+    messages: {
+      ui: {
+        popover: {
+          Filter: "探す",
+          "Nothing found": "見つかりません",
+        },
+      },
+      blockTunes: {
+        delete: {
+          Delete: "削除",
+        },
+        moveUp: {
+          "Move up": "上へ",
+        },
+        moveDown: {
+          "Move down": "下へ",
+        },
+      },
+      toolNames: {
+        Text: "テキスト",
+        Heading: "見出し",
+        List: "リスト",
+        Link: "リンク",
+        Bold: "太字",
+        Italic: "斜体",
+      },
+    },
+  },
+});
